7a15b5fd219d74329171be5aca7b105aa718e2cb
AlThorStormblessed <anshgudibanda@gmail.com>
2024-02-16 17:13:42 +0530

    Added files

---
+++ create_video.py
@@ -0,0 +1,65 @@
+# importing libraries 
+import os, glob
+import cv2 
+from PIL import Image 
+
+# Checking the current directory path 
+print(os.getcwd()) 
+
+# Folder which contains all the images 
+# from which video is to be generated 
+path = f"num_episodes_30000/"
+
+mean_height = 0
+mean_width = 0
+
+num_of_images = len(os.listdir(path)) 
+# print(num_of_images) 
+
+for file in os.listdir(path): 
+	im = Image.open(os.path.join(path, file)) 
+	width, height = im.size 
+	mean_width += width 
+	mean_height += height 
+	# im.show() # uncomment this for displaying the image 
+
+# Finding the mean height and width of all images. 
+# This is required because the video frame needs 
+# to be set with same width and height. Otherwise 
+# images not equal to that width height will not get 
+# embedded into the video 
+mean_width = int(mean_width / num_of_images) 
+mean_height = int(mean_height / num_of_images) 
+ 
+def generate_video(): 
+	images = [img for img in os.listdir(path)] 
+	
+	# Array images should only consider 
+	# the image files ignoring others if any 
+	images = sorted(images, key= lambda x: int(x[:-4]))
+	print(images)
+
+	frame = cv2.imread(os.path.join(path, images[0])) 
+
+	# setting the frame width, height width 
+	# the width, height of first image 
+	height, width, layers = frame.shape 
+
+	video = cv2.VideoWriter(
+    f"{path[:-1]}.mp4",
+    cv2.VideoWriter_fourcc(*"MP4V"),
+    24,
+    (width, height),
+)
+
+	# Appending the images to the video one by one 
+	for image in images: 
+		video.write(cv2.imread(os.path.join(path, image))) 
+	
+	# Deallocating memories taken for window creation 
+	cv2.destroyAllWindows() 
+	video.release() # releasing the video generated 
+
+
+# Calling the generate_video function 
+generate_video() 
---
+++ flappy_gravity.py
@@ -0,0 +1,312 @@
+import matplotlib.pyplot as plt
+import pygame, random, time, math, numpy as np, pickle
+from pygame.locals import *
+from matplotlib import style
+
+style.use("ggplot")
+
+
+#VARIABLES
+SCREEN_WIDTH = 300
+SCREEN_HEIGHT = 600
+n = 3
+SPEED = 60
+GRAVITY = 10
+GAME_SPEED = 30
+
+
+GROUND_WIDTH = 2 * SCREEN_WIDTH
+GROUND_HEIGHT= 100
+
+PIPE_WIDTH = 80
+PIPE_HEIGHT = 500
+
+PIPE_GAP = 150
+
+wing = 'assets/audio/wing.wav'
+hit = 'assets/audio/hit.wav'
+
+pygame.mixer.init()
+
+
+class Bird(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.images =  [pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha(),
+                        pygame.image.load('assets/sprites/bluebird-midflap.png').convert_alpha(),
+                        pygame.image.load('assets/sprites/bluebird-downflap.png').convert_alpha()]
+
+        self.speed = SPEED
+
+        self.current_image = 0
+        self.image = pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha()
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = SCREEN_WIDTH / 6
+        self.rect[1] = SCREEN_HEIGHT / 2
+
+    def update(self):
+        self.current_image = (self.current_image + 1) % 3
+        self.image = self.images[self.current_image]
+        self.speed += GRAVITY
+        # self.speed = min(min(0, self.speed) + GRAVITY, SPEED)
+
+        #UPDATE HEIGHT
+        self.rect[1] += self.speed
+
+    def bump(self):
+        self.speed = -SPEED
+
+    def begin(self):
+        self.current_image = (self.current_image + 1) % 3
+        self.image = self.images[self.current_image]
+
+class Pipe(pygame.sprite.Sprite):
+
+    def __init__(self, inverted, xpos, ysize):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (PIPE_WIDTH, PIPE_HEIGHT))
+
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+
+        if inverted:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect[1] = - (self.rect[3] - ysize)
+        else:
+            self.rect[1] = SCREEN_HEIGHT - ysize
+
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+        
+
+class Ground(pygame.sprite.Sprite):
+    
+    def __init__(self, xpos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('assets/sprites/base.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (GROUND_WIDTH, GROUND_HEIGHT))
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+def is_off_screen(sprite):
+    return sprite.rect[0] < -(sprite.rect[2])
+
+def get_random_pipes(xpos):
+    size = random.randint(20, 30) * 10
+    pipe = Pipe(False, xpos, size)
+    pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
+    flag_ = True
+    return pipe, pipe_inverted
+
+def disc_pos(pos):
+    pos = int(pos//5) * 5
+    return pos
+    
+def imp(pos):
+    pos = int(pos//10) * 10
+    return pos
+
+num_ep = 10000
+score_rew = 15
+flag_ = True
+crash = -1000
+over_flow = -1000
+epsilon = 0.
+epsilon_decay = .98
+alpha = 0.05
+alpha_decay = 0.9998
+discount = 1
+show_every = 200
+peak_score = 0
+scores = []
+q_table = {}
+for i in range(600, -151, -10):
+    for ii in range(-400, 600, 5):
+        for iv in range(-80, 100, 10):
+            q_table[(i, ii, iv)] = [0 for i in range(3)]
+
+for i in range(num_ep):
+    if i % show_every == 0:
+        show = True
+    else:
+        show = False
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Flappy Bird')
+
+    BACKGROUND = pygame.image.load('assets/sprites/background-day.png')
+    BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    bird_group = pygame.sprite.Group()
+    bird = Bird()
+    bird_group.add(bird)
+
+    ground_group = pygame.sprite.Group()
+
+    for j in range (2):
+        ground = Ground(GROUND_WIDTH * j)
+        ground_group.add(ground)
+
+    pipe_group = pygame.sprite.Group()
+    for j in range(2):
+        pipes = get_random_pipes(SCREEN_WIDTH * j + 600)
+        pipe_group.add(pipes[0])
+        pipe_group.add(pipes[1])
+
+    clock = pygame.time.Clock()
+    score = 0
+    begin = True
+
+    while begin:
+
+        if not show:
+            clock.tick(10000)
+        else:
+            clock.tick(15)
+
+        bird.bump()
+        if show:
+            pygame.mixer.music.load(wing)
+            pygame.mixer.music.play()
+
+        begin = False
+
+        screen.blit(BACKGROUND, (0, 0))
+
+        if is_off_screen(ground_group.sprites()[0]):
+            ground_group.remove(ground_group.sprites()[0])
+
+            new_ground = Ground(GROUND_WIDTH - 20)
+            ground_group.add(new_ground)
+
+        bird.begin()
+        ground_group.update()
+
+        if show:
+            bird_group.draw(screen)
+            ground_group.draw(screen)
+
+        pygame.display.update()
+
+    total_reward = 0
+    for k in range(10000):
+        obs = (imp(pipe_group.sprites()[1].rect[0]), imp(pipe_group.sprites()[0].rect[1] - bird.rect[1]), bird.speed)
+
+        if not show:
+            clock.tick(10000)
+        else:
+            clock.tick(12)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+
+        if(np.random.random() < epsilon): action = np.random.randint(0, 2)
+        else: 
+            action = np.argmax(q_table[obs][:2])
+            # print("Action")
+
+        if(action): 
+            bird.bump()
+            if show:
+                pygame.mixer.music.load(wing)
+                pygame.mixer.music.play()
+        q_table[obs][2] += 1
+
+        screen.blit(BACKGROUND, (0, 0))
+
+        if is_off_screen(ground_group.sprites()[0]):
+            ground_group.remove(ground_group.sprites()[0])
+
+            new_ground = Ground(GROUND_WIDTH - 20)
+            ground_group.add(new_ground)
+
+        if is_off_screen(pipe_group.sprites()[0]):
+            pipe_group.remove(pipe_group.sprites()[0])
+            pipe_group.remove(pipe_group.sprites()[0])
+
+            pipes = get_random_pipes(SCREEN_WIDTH * 2)
+
+            pipe_group.add(pipes[0])
+            pipe_group.add(pipes[1])
+
+        bird_group.update()
+        ground_group.update()
+        pipe_group.update()
+
+        if show:
+            bird_group.draw(screen)
+            pipe_group.draw(screen)
+            ground_group.draw(screen)
+
+        pygame.display.update()
+
+        # print(bird.speed)
+
+        if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
+                pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
+            reward = crash
+        elif(bird.rect[1] < 0):
+            reward = over_flow
+
+        else:
+            # print(pipe_group.sprites()[0].rect)
+            if(pipe_group.sprites()[0].rect[0] < bird.rect[0] and flag_):
+                score += 1
+                flag_ = False
+                peak_score = max(peak_score, score)
+                reward = score_rew
+            elif(pipe_group.sprites()[0].rect[0] > bird.rect[0]):
+                flag_ = True
+                reward = 15
+            else:
+                reward = 15
+        
+        alpha = 1/(1 + q_table[obs][2])
+        new_obs = (imp(pipe_group.sprites()[1].rect[0]), pipe_group.sprites()[0].rect[1] - disc_pos(bird.rect[1]), bird.speed)
+        max_future_q = np.max(q_table[new_obs][:2])
+        current_q = q_table[obs][action]
+        
+        new_q = (1 - alpha) * current_q + alpha * (reward + discount * max_future_q)
+        q_table[obs][action] = new_q
+
+        total_reward += reward
+
+        if(reward == crash or reward == over_flow):
+            if show:
+                pygame.mixer.music.load(wing)
+                pygame.mixer.music.play()
+            break
+    
+    scores.append(score)
+    print(f"{i + 1}th Episode: Reward = {total_reward}, Peak Score = {peak_score}, Score = {score}, Rolling Average = {round(np.mean(scores[-200:]), 4)}")
+    epsilon *= epsilon_decay
+    # alpha *= alpha_decay  
+
+with open(f"qtable-{int(time.time())}.pickle", "wb") as f:
+    pickle.dump(q_table, f)
+
+plt.scatter(np.arange(1, num_ep), scores, s = 0.1)
+plt.grid()
+plt.xlabel("Number of episodes")
+plt.ylabel("Score")
+plt.show()
